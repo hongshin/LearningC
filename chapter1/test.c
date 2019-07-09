49f26de81ab558f2b96a595de50a969b2177f11c
Shin Hong <hongshin@handong.edu>
2019-07-09 12:42:02 +0900

    Add test.c

---
+++ chapter1/test.c
@@ -0,0 +1,14 @@
+/* test.c
+ *
+ * This is a program as a test input for Exercise 2 
+ * 
+ */
+
+ int
+ main () // This is a main function
+ {
+ 	printf("Hello \n" /* Not Hi */) ;
+	printf("C has two ways for users to write comments: /* */ and //\n") ;
+	/*
+	End of program */
+ }
