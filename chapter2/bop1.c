a057e93d98a31ef51091db3d8d554b7db4c67297
Shin Hong <hongshin@handong.edu>
2019-07-10 14:45:29 +0900

    Update

--- chapter2/bop1.c
+++ chapter2/bop1.c
@@ -7,12 +7,11 @@ int
 main ()
 {
 	unsigned int n = 0xabcd ;
-	printf("%d\n", n) ;
-	printf("%x\n", n & 0xff00) ;
-	printf("%x\n", n & ~0xf) ;
-
-	printf("%x\n", n >> 4) ;
-	//printf("%x\n", getbits(n, 4, 8)) ;
+	printf("%x\n", n) ;
+	//printf("%x\n", n & 0xff00) ;
+	//printf("%x\n", n & ~0xf) ;
+	//printf("%x\n", n >> 4) ;
+	printf("%x\n", getbits(n, 4, 8)) ;
 }
 
 unsigned int getbits(unsigned int x, int p, int n) 
@@ -20,7 +19,7 @@ unsigned int getbits(unsigned int x, int p, int n)
 	return (x >> p ) & ~(~0 << n) ;
 }
 
-void setbit(unsigned int x, int b, int v)
+unsigned int setbit(unsigned int x, int b, int v)
 {
 	/* TODO */
 }
