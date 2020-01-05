e3a37e227f7247e84e6bf9cf186a273c0e7995ab
Shin Hong <hongshin@handong.edu>
2020-01-06 00:28:19 +0900

    Add Rushhour

---
+++ rushhour/rushour.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* NOFIX --- */
+
+typedef enum {
+	start,
+	left,
+	right,
+	up,
+	down,
+	quit,
+	N_op 
+} commands ;
+
+typedef enum {
+	vertical,
+	horizontal 
+} direction ;
+
+typedef struct {
+	int id ;
+	int y1, y2 ;	// y1: the minimum of y, y2: the maximum of y
+	int x1, x2 ;	// x1: the minimum of x, x2: the maximum of x
+	int span ;		// the number of cells 
+	direction dir ;	// the direction of the car
+} car_t ;
+
+char * op_str[N_op] = {
+	"start",
+	"left",
+	"right",
+	"up",
+	"down",
+	"quit"
+} ;
+
+int n_cars = 0 ;
+car_t * cars = 0x0 ;
+int cells[6][6] ;
+
+/* --- NOFIX */
+
+
+commands
+get_op_code (char * s)
+{
+	// return the corresponding number for the command given as s.
+	// FIXME
+}
+
+int
+load_game (char * filename)
+{
+	//FIXME
+	// load_game returns 0 for a success, or return 1 for a failure.
+	// Use fopen, getline, strtok, atoi, strcmp
+	// Note that the last character of a line obtained by getline may be '\n'.
+}
+
+void
+display ()
+{
+	/* The beginning state of board1.txt must be shown as follows: 
+ 	 + + 2 + + +
+ 	 + + 2 + + +
+	 1 1 2 + + +
+	 3 3 3 + + 4
+	 + + + + + 4
+	 + + + + + 4
+	*/
+
+	//FIXME
+}
+
+int 
+update_cells ()
+{
+	memset(cells, 0, sizeof(int) * 36) ; // clear cells before the write.
+
+	//FIXME
+	// return 0 for sucess
+	// return 1 if the given car information (cars) has a problem
+}
+
+int
+move (int id, int op) 
+{
+	//FIXME
+	// move returns 1 when the given input is invalid.
+	// or return 0 for a success.
+
+	// Update cars[id].x1, cars[id].x2, cars[id].y1 and cars[id].y2
+	//   according to the given command (op) if it is possible.
+
+	// The condition that car_id can move left is when 
+	//  (1) car_id is horizontally placed, and
+	//  (2) the minimum x value of car_id is greather than 0, and
+	//  (3) no car is placed at cells[cars[id].y1][cars[id].x1-1].
+	// You can find the condition for moving right, up, down as
+	//   a similar fashion.
+}
+
+int
+main ()
+{
+	char buf[128] ;
+	int op ;
+	int id ;
+
+	while (1) {
+		scanf("%s", buf) ;
+
+		switch (op = get_op_code(buf)) {
+			case start:
+				scanf("%s", buf) ;
+				load_game(buf) ;
+				update_cells() ;
+				display() ;
+
+			case left:
+			case right:
+			case up:
+			case down:
+				scanf("%d", &id) ;
+				move(id, op) ;
+				update_cells() ;
+				display() ;
+			//FIXME
+		}
+	}
+}
